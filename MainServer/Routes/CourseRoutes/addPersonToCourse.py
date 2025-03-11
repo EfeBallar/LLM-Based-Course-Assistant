aa17aced3b12d0989bf2443b84fe3ce634e73f89
goktug-aygun <goktug.aygun@sabanciuniv.edu>
2025-03-12 00:03:34 +0300

    Status notification and role update instruction

--- MainServer/Routes/CourseRoutes/addPersonToCourse.py
+++ MainServer/Routes/CourseRoutes/addPersonToCourse.py
@@ -10,23 +10,26 @@ def add_person_to_course(db):
         course_code = request.json.get('course_code')
 
         if not user_name or not course_code:
-            return jsonify({"error": "Username and course_code are required"}), 400
+            return jsonify({"status": 0, "message": "Username and course_code are required"}), 401
         
         # Find the user by user_name
         user = db.Users.find_one({"email": f"{user_name}@sabanciuniv.edu"})
         if not user:
-            return jsonify({"error": "User not found"}), 404
+            return jsonify({"status": 0, "message": "User not found"}), 200
        
         course = db.Courses.find_one({"courseCode": course_code})
         if not course:
-            return jsonify({"error": "Course not found"}), 404
+            return jsonify({"status": 0, "message": "Course not found"}), 200
         
         if (user["_id"] not in course["personnel_ids"]):    # if they are not already authorized
             
-            # Add the course code to user's auth_courses array
+            # Add the course code to user's auth_courses array and set role to "Instructor"
             db.Users.update_one(
                 {"_id": ObjectId(user["_id"])},
-                {"$addToSet": {"auth_courses": course_code}} # No duplicates allowed
+                {
+                    "$addToSet": {"auth_courses": course_code},  # No duplicates allowed
+                    "$set": {"role": "Instructor"}
+                }
             )
 
             # Add the user_id to course's personnel array
